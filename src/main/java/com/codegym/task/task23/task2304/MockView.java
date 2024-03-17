3a7929ca0539948335a0c77506f373149c221bbc
mc227 <markcostales.mark@gmail.com>
2024-03-16 21:49:42 -0400

    3 time

--- src/main/java/com/codegym/task/task23/task2304/MockView.java
+++ src/main/java/com/codegym/task/task23/task2304/MockView.java
@@ -4,14 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MockView {
-    private static final Map FAKE_TASK_CRITERIA = new HashMap();
     private static final Map FAKE_NAME_CRITERIA = new HashMap();
-
-    public static Map getFakeTaskCriteria(){
-        return FAKE_TASK_CRITERIA;
-    }
+    private static final Map FAKE_TASK_CRITERIA = new HashMap();
 
     public static Map getFakeNameCriteria(){
         return FAKE_NAME_CRITERIA;
+    }
+
+    public  static Map getFakeTaskCriteria(){
+        return FAKE_TASK_CRITERIA;
     }
 }
