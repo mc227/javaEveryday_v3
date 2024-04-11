6c7868bf3762a3c003713b4c15388a5442490405
mc227 <markcostales.mark@gmail.com>
2024-04-11 09:55:34 -0400

    DooBee class added

---
+++ src/main/java/hfj3e/DooBee.java
@@ -0,0 +1,17 @@
+package hfj3e;
+
+import java.util.Arrays;
+
+public class DooBee {
+    public static void main(String[] args) {
+        int x = 1;
+        while (x < 3) {
+            System.out.print("Doo");
+            System.out.print("Bee");
+            x = x + 1;
+        }
+        if (x == 3) {
+            System.out.print("Do");
+        }
+    }
+}
---
+++ src/main/java/hfj3e/IfTest2.java
@@ -0,0 +1,13 @@
+package hfj3e;
+
+public class IfTest2 {
+    public static void main(String[] args) {
+        int x = 2;
+        if (x == 3){
+            System.out.println("x = 3");
+        } else {
+            System.out.println("x is NOT 3");
+        }
+        System.out.println("This prints no matter what");
+    }
+}
